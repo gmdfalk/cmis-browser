874718b3fcc06a3b9970a9e2e8e66917b28cab22
Max Falk <gmdfalk@gmail.com>
2015-10-22 23:05:33 +0200

    Adjust integration test

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -20,7 +20,7 @@ describe('my app', function() {
 
     it('should render view1 when user navigates to /view1', function() {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+        toMatch(/Hello/);
     });
 
   });
