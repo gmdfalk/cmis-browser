5bcd4d2b709128e9e908812279280d3e4fca5eca
Max Falk <gmdfalk@gmail.com>
2015-10-25 18:52:22 +0100

    Fix integration test by removing bootstrap module

--- app/app.js
+++ app/app.js
@@ -5,8 +5,7 @@ angular.module("myApp", [
   "ngRoute",
   "myApp.view1",
   "myApp.view2",
-  "myApp.version",
-  "ui.bootstrap"
+  "myApp.version"
 ]).
 config(["$routeProvider", function($routeProvider) {
   $routeProvider.otherwise({redirectTo: "/view1"});
