e81f3c473e69ca0858e273a8f5a3f0b4065e44dc
Max Falk <gmdfalk@gmail.com>
2015-11-01 19:06:42 +0100

    Add angular-filemenager

--- app/app.js
+++ app/app.js
@@ -5,8 +5,27 @@ angular.module("myApp", [
   "ngRoute",
   "myApp.view1",
   "myApp.view2",
-  "myApp.version"
-]).
+  "myApp.version",
+  "ui.bootstrap"
+])
+.
 config(["$routeProvider", function($routeProvider) {
   $routeProvider.otherwise({redirectTo: "/view1"});
+}]);
+
+
+.controller("View1Ctrl", ["$scope", function($scope) {
+	$scope.greeting = "Hello";
+	$scope.double = function(num) { return num * 2; };
+//	var cmis = require("cmis");
+//	var url = "http://cmis.alfresco.com/cmisbrowser";
+//	var username = "admin";
+//	var password = "admin";
+//	var session = cmis.createSession(url);
+//	session.setGlobalHandlers(console.log, console.log);
+//	session.setCredentials(username, password);
+//	session.loadRepositories();
+//	console.log(session.getRepositoryInfo());
+//	var supportedVersion = session.defaultRepository.cmisVersionSupported;
+//	console.log(supportedVersion);
 }]);
