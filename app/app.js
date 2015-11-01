2e9a2b9dfd13baededd1666c1af7868d77eae09d
Max Falk <gmdfalk@gmail.com>
2015-11-01 19:49:56 +0100

    Include bootstrap

--- app/app.js
+++ app/app.js
@@ -8,24 +8,24 @@ angular.module("myApp", [
   "myApp.version",
   "ui.bootstrap"
 ])
-.
-config(["$routeProvider", function($routeProvider) {
+
+.config(["$routeProvider", function($routeProvider) {
   $routeProvider.otherwise({redirectTo: "/view1"});
-}]);
+}])
 
 
 .controller("View1Ctrl", ["$scope", function($scope) {
 	$scope.greeting = "Hello";
 	$scope.double = function(num) { return num * 2; };
-//	var cmis = require("cmis");
-//	var url = "http://cmis.alfresco.com/cmisbrowser";
-//	var username = "admin";
-//	var password = "admin";
-//	var session = cmis.createSession(url);
-//	session.setGlobalHandlers(console.log, console.log);
-//	session.setCredentials(username, password);
-//	session.loadRepositories();
-//	console.log(session.getRepositoryInfo());
-//	var supportedVersion = session.defaultRepository.cmisVersionSupported;
-//	console.log(supportedVersion);
+// var cmis = require("cmis");
+// var url = "http://cmis.alfresco.com/cmisbrowser";
+// var username = "admin";
+// var password = "admin";
+// var session = cmis.createSession(url);
+// session.setGlobalHandlers(console.log, console.log);
+// session.setCredentials(username, password);
+// session.loadRepositories();
+// console.log(session.getRepositoryInfo());
+// var supportedVersion = session.defaultRepository.cmisVersionSupported;
+// console.log(supportedVersion);
 }]);
